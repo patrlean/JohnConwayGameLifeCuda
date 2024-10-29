81fb11b31b2590835bcc4c21d37ed42b66860e33
patrlean <tianyouzhaouestc@163.com>
2024-10-29 13:17:21 -0400

    add files

---
+++ CUDA_SFML/include/parameterParse.hpp
@@ -0,0 +1,11 @@
+#ifndef PARAMETERPARSE_HPP
+#define PARAMETERPARSE_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "Globals.hpp"
+using namespace std;
+
+
+#endif
---
+++ CUDA_SFML/src/parameterParse.cpp
@@ -0,0 +1,46 @@
+#include "parameterParse.hpp"
+
+using namespace std;
+
+void parseArgs(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++) {
+        string arg = argv[i];
+
+        if (arg == "-n" && i + 1 < argc) {
+            numThreads = atoi(argv[++i]); // no less than 2
+            if (numThreads < 2){
+                cerr << "Error: Invalid number of threads.\n Number of threads must be greater than 1." << endl;
+                exit(EXIT_FAILURE);  // exit
+            }
+        } else if (arg == "-c" && i + 1 < argc) {
+            cellSize = atoi(argv[++i]);   // no less than 1
+            if (cellSize < 1){
+                cerr << "Error: Invalid cell size.\n Cell size must be greater than 0." << endl;
+                exit(EXIT_FAILURE);  // exit
+            }
+        } else if (arg == "-x" && i + 1 < argc) {
+            windowWidth = atoi(argv[++i]);
+            if (windowWidth < 0){
+                cerr << "Error: Invalid window width." << endl;
+                exit(EXIT_FAILURE);  // exit
+            }
+        } else if (arg == "-y" && i + 1 < argc) {
+            windowHeight = atoi(argv[++i]);
+            if (windowHeight < 0){
+                cerr << "Error: Invalid window height." << endl;
+                exit(EXIT_FAILURE);  // exit
+            }
+        } else if (arg == "-t" && i + 1 < argc) {
+            processingType = argv[++i];
+            // check processingType valid?
+            if (processingType != "SEQ" && processingType != "THRD" && processingType != "OMP") {
+                cerr << "Error: Invalid processing type. Must be one of SEQ, THRD, OMP." << endl;
+                exit(EXIT_FAILURE);  // exit
+            }
+        } else {
+            cerr << "Usage: " << argv[0] << " [-n numThreads] [-c cellSize] [-x windowWidth] [-y windowHeight] [-t processingType]" << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+}
